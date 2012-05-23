942045528754117e3d3f8e6c3ef4d8283f374028
GaryRudolph <GaryRudolph@mac.com>
2012-05-23 11:28:12 -0700

    Updated with detail call and tracking

--- citygrid.py
+++ citygrid.py
@@ -2,11 +2,77 @@ import logging
 import webapp2
 import urllib2
 import json
+import uuid
 
 publisher = "4214549098"
 placement = "monitor"
 
 class CityGridHandler(webapp2.RequestHandler):
+	def get_search_location(self, what, where, listing_id,):
+		logger = logging.getLogger("citygrid.CityGridHandler.get_search_location")
+		logger.debug("ENTER what=%(what)s, where=%(where)s, listing_id=%(listing_id)s", {"what": what, "where": where, "listing_id": listing_id})
+
+		url = "http://api.citygridmedia.com/content/places/v2/search/where?what=%(what)s&client_ip=%(remote_address)s&where=%(where)s&publisher=%(publisher)s&placement=%(placement)s&format=json" %\
+			  {"what": urllib2.quote(what), "where": urllib2.quote(where), "publisher": publisher, "placement": placement, "remote_address": self.request.remote_addr}
+		logger.debug("url=%(url)s", {"url": url})
+
+		fp = urllib2.urlopen(url)
+		parsed = json.load(fp)
+
+		logger.debug("parsed=%(parsed)s", {"parsed": parsed})
+
+		location = None
+		for location in parsed["results"]["locations"]:
+			logger.debug("location id=%(id)s, name=%(name)s", {"id": location["id"], "name": location["name"]})
+			if listing_id == location["id"]:
+				location = location
+				break
+		return location
+
+	def get_detail_location(self, search_location):
+		logger = logging.getLogger("citygrid.CityGridHandler.get_detail_location")
+		logger.debug("ENTER search_location=%(search_location)s", {"search_location": search_location})
+
+		if search_location == None:
+			return None
+
+		listing_id = search_location["id"]
+		impression_id = search_location["impression_id"]
+
+		url = "http://api.citygridmedia.com/content/places/v2/detail?client_ip=%(remote_address)s&placement=%(placement)s&all_results=false&publisher=%(publisher)s&i=%(impression_id)s&listing_id=%(listing_id)s&format=json" %\
+			  {"remote_address": self.request.remote_addr, "placement": placement, "publisher": publisher, "impression_id": impression_id, "listing_id": listing_id}
+		logger.debug("url=%(url)s", {"url": url})
+
+		fp = urllib2.urlopen(url)
+		parsed = json.load(fp)
+
+		logger.debug("parsed=%(parsed)s", {"parsed": parsed})
+
+		location = parsed["locations"][0]
+		return location
+
+	def track_location(self, detail_location, action_target):
+		logger = logging.getLogger("citygrid.CityGridHandler.track_location")
+		logger.debug("ENTER detail_location=%(detail_location)s, action_target=%(action_target)s", {"detail_location": detail_location, "action_target": action_target})
+
+		if detail_location == None:
+			return False
+
+		listing_id = detail_location["id"]
+		impression_id = detail_location["impression_id"]
+		reference_id = detail_location["reference_id"]
+		device_id = str(uuid.uuid4())
+
+		url = "http://api.citygridmedia.com/ads/tracker/imp?reference_id=%(reference_id)s&placement=%(placement)s&mobile_type=iPhone%%20Monitor&muid=%(device_id)s&publisher=%(publisher)s&i=%(impression_id)s&listing_id=%(listing_id)s&action_target=%(action_target)s" %\
+			  {"reference_id": reference_id, "placement": placement, "device_id": device_id, "publisher": publisher, "impression_id": impression_id, "listing_id": listing_id, "action_target": action_target}
+		logger.debug("url=%(url)s", {"url": url})
+
+		data = urllib2.urlopen(url).read()
+		if data != None:
+			return True
+		else:
+			return False
+
 	def get(self):
 		logger = logging.getLogger("citygrid.CityGridHandler.get")
 		logger.debug("ENTER")
@@ -15,32 +81,39 @@ class CityGridHandler(webapp2.RequestHandler):
 		where=self.request.get("where")
 		listing_id=int(self.request.get("listing_id"))
 
-		logger.debug("what=%(what)s, where=%(where)s, listing_id=%(listing_id)s", {"what": what, "where": where, "listing_id": listing_id})
-
-		cg_search_url = "http://api.citygridmedia.com/content/places/v2/search/where?what=%(what)s&client_ip=%(remote_address)s&where=%(where)s&publisher=%(publisher)s&placement=%(placement)s&format=json" % {"what": urllib2.quote(what), "where": urllib2.quote(where), "publisher": publisher, "placement": placement, "remote_address": self.request.remote_addr}
-		logger.debug("cg_search_url=%(cg_search_url)s" % {"cg_search_url": cg_search_url})
-
-		cg_search_fp = urllib2.urlopen(cg_search_url)
-		cg_search_parsed = json.load(cg_search_fp)
-
-		logger.debug("cg_search_parsed=%(cg_search_parsed)s", {"cg_search_parsed": cg_search_parsed})
-
-		cg_search_location = None
-		for location in cg_search_parsed["results"]["locations"]:
-			logger.debug("location id=%(id)s, name=%(name)s", {"id": location["id"], "name": location["name"]})
-			if listing_id == location["id"]:
-				cg_search_location = location
-				break
-
-		if cg_search_location != None:
-			logger.debug("cg_search_location=%(cg_search_location)s", {"cg_search_location": cg_search_location})
+		search_location = self.get_search_location(what, where, listing_id)
+		if search_location != None:
+			logger.debug("search_location=%(search_location)s", {"search_location": search_location})
 		else:
-			self.response.out.write("Location %(listing_id)s not found" % {"listing_id": listing_id})
+			self.response.out.write("Search Location %(listing_id)s not found" % {"listing_id": listing_id})
 			return
 
-#http://api.citygridmedia.com/content/places/v2/detail?client_ip=127.0.0.1&customer_only=false&placement=demo-sdk&format=json&all_results=false&publisher=4214549098&i=000b0000001b45849161d94dd5b9dfb07d727bc84e&listing_id=603514592
-#http://api.citygridmedia.com/ads/tracker/imp?reference_id=1&dialPhone=3102782050&placement=demo-sdk&mobile_type=iPhone%20Simulator&muid=B4B53725-EBAC-5A7A-9BB9-9B6BEADB023C&format=json&publisher=4214549098&i=0009000000212f149d8d164873bd8d56340495abcc&listing_id=603514592&action_target=listing_profile
-#http://api.citygridmedia.com/ads/tracker/imp?reference_id=1&dialPhone=3102782050&placement=demo-sdk&mobile_type=iPhone%20Simulator&muid=B4B53725-EBAC-5A7A-9BB9-9B6BEADB023C&format=json&publisher=4214549098&i=0009000000212f149d8d164873bd8d56340495abcc&listing_id=603514592&action_target=listing_review
-#http://api.citygridmedia.com/ads/tracker/imp?reference_id=1&dialPhone=3102782050&placement=demo-sdk&mobile_type=iPhone%20Simulator&muid=B4B53725-EBAC-5A7A-9BB9-9B6BEADB023C&format=json&publisher=4214549098&i=0009000000212f149d8d164873bd8d56340495abcc&listing_id=603514592&action_target=listing_map
+		detail_location = self.get_detail_location(search_location)
+		if detail_location != None:
+			logger.debug("detail_location=%(detail_location)s", {"detail_location": detail_location})
+		else:
+			self.response.out.write("Detail Location %(search_location)s not found" % {"search_location": search_location})
+			return
+
+		tracked_profile = self.track_location(detail_location, "listing_profile")
+		if tracked_profile:
+			logger.debug("profile tracked")
+		else:
+			self.response.out.write("Profile tracking failed for %(detail_location)s not found" % {"detail_location": detail_location})
+			return
+
+		tracked_review = self.track_location(detail_location, "listing_review")
+		if tracked_review:
+			logger.debug("review tracked")
+		else:
+			self.response.out.write("Review tracking failed for %(detail_location)s not found" % {"detail_location": detail_location})
+			return
+
+		tracked_map = self.track_location(detail_location, "listing_map")
+		if tracked_map:
+			logger.debug("map tracked")
+		else:
+			self.response.out.write("Map tracking failed for %(detail_location)s not found" % {"detail_location": detail_location})
+			return
 
 		self.response.out.write("Success")
